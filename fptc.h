bda01a3056ade8fb1c43fb14bc0edacd21462b1a
mgetka <michal.getka@gmail.com>
2017-12-22 11:54:33 +0100

    minor changes

--- fptc.h
+++ fptc.h
@@ -130,7 +130,7 @@ typedef __uint128_t fptud;
 
 #define FPT_ONE       ((fpt)((fpt)1 << FPT_FBITS))
 #define FPT_ZERO      ((fpt)0)
-#define FPT_MINUS_ONE ((fpt)0 - FPT_ONE)
+#define FPT_MINUS_ONE (-FPT_ONE)
 #define FPT_ONE_HALF  (FPT_ONE >> 1)
 #define FPT_TWO       (FPT_ONE + FPT_ONE)
 #define FPT_MAX       ((fpt)((fptu)~0 >> 1))
